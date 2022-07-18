d3666f3583a5ea2ec958d1a62eaeaac324dbc47d
XcQuid <creed.35@yandex.ru>
2022-07-18 17:25:32 +0300

    Updates

--- src/state/action-types/index.ts
+++
@@ -1,8 +0,0 @@
-export enum ActionType {
-  FETCH_STANDING = 'fetch_standing',
-  FETCH_STANDING_SUCCESS = 'fetch_standing_success',
-  FETCH_STANDING_ERROR = 'fetch_standing_error',
-  FETCH_SCHEDULE = 'fetch_schedule',
-  FETCH_SCHEDULE_SUCCESS = 'fetch_schedule_success',
-  FETCH_SCHEDULE_ERROR = 'fetch_schedule_error',
-}
