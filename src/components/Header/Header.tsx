e91760bad049982d47504faf75c178ff9491f1dd
XcQuid <creed.35@yandex.ru>
2022-07-20 10:11:49 +0300

    feat: add navbar links in Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,18 @@
 import './Header.scss';
 import React, { ReactElement } from 'react';
+import { NavLink } from 'react-router-dom';
 
 export default function Header(): ReactElement {
   return (
     <header className="header">
       <div className="header__container">
-        <div className="header__logo" />
+        <nav className="header__navbar">
+          <NavLink to="/schedule" className="header__link">Schedule</NavLink>
+          <NavLink to="/standing" className="header__link">Standing</NavLink>
+        </nav>
+        <div className="header__logo-wrapper">
+          <div className="header__logo" />
+        </div>
       </div>
     </header>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
 import { store } from './state';
 import './styles/index.scss';
@@ -12,8 +13,10 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
 );
