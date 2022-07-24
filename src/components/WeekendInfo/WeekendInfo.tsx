beef0ab39cedf22e4c67a6e11c94074d7e462624
XcQuid <creed.35@yandex.ru>
2022-07-24 14:02:34 +0300

    refactor: delete CIrcuitInfo component

--- src/components/WeekendInfo/WeekendInfo.tsx
+++ src/components/WeekendInfo/WeekendInfo.tsx
@@ -5,12 +5,10 @@ import placeholder from '../../images/F1-logo.svg';
 import { refactorWeekendDates } from '../../utils/utils';
 import { useTypedSelector } from '../../hooks/useTypedSelector';
 import Session from '../Session/Session';
-import CircuitInfo from '../CircuitInfo/CircuitInfo';
 import { useActions } from '../../hooks/useActions';
 
 const WeekendInfo: React.FC = () => {
   const { fetchWikiData } = useActions();
-  const [circuitImage, setCircuitImage] = useState('');
 
   const weekend = useTypedSelector((state) => state.weekend);
   const { loading, wikiData, error } = useTypedSelector((state) => state.wikiData);
@@ -27,61 +25,71 @@ const WeekendInfo: React.FC = () => {
   return (
     <div className="weekend-info">
       <h3 className="weekend-info__header">{weekend?.raceName}</h3>
-      <img
-        className="weekend-info__image"
-        src={wikiData ? wikiData.imgSource : placeholder}
-        alt={weekend?.Circuit.circuitName}
-      />
+      <figure className="weekend-info__figure">
+        <img
+          className="weekend-info__image"
+          src={wikiData ? wikiData.imgSource : placeholder}
+          alt={weekend?.Circuit.circuitName}
+        />
+        <h5 className="weekend-info__circuit-name">
+          {weekend?.Circuit.circuitName}
+        </h5>
+      </figure>
+      <div className="weekend-info__circuit-description">
+        <p className="weekend-info__circuit-info">
+          Country: <span>{weekend?.Circuit.Location.country}</span>
+        </p>
+        <p className="weekend-info__circuit-info">
+          Locality: <span>{weekend?.Circuit.Location.locality}</span>
+        </p>
+      </div>
       { weekend && (
-        <>
-          <ul className="weekend-info__dates">
-            <Session
-              title="FP1"
-              date={dates.firstPractice.date}
-              time={dates.firstPractice.time}
-              type="practice"
-            />
-            <Session
-              title="FP2"
-              date={dates.secondPractice.date}
-              time={dates.secondPractice.time}
-              type="practice"
-            />
-            {
-              weekend.ThirdPractice && (
-                <Session
-                  title="FP3"
-                  date={dates.thirdPractice!.date}
-                  time={dates.thirdPractice!.time}
-                  type="practice"
-                />
-              )
-            }
-            {
-              weekend.Sprint && (
-                <Session
-                  title="Sprint"
-                  date={dates.sprint!.date}
-                  time={dates.sprint!.time}
-                  type="sprint"
-                />
-              )
-            }
-            <Session
-              title="QU"
-              date={dates.qualifying.date}
-              time={dates.qualifying.time}
-              type="qualifying"
-            />
-            <Session
-              title="RACE"
-              date={dates.race.date}
-              time={dates.race.time}
-              type="race"
-            />
-          </ul>
-          <CircuitInfo weekend={weekend} />
-        </>
+        <ul className="weekend-info__dates">
+          <Session
+            title="FP1"
+            date={dates.firstPractice.date}
+            time={dates.firstPractice.time}
+            type="practice"
+          />
+          <Session
+            title="FP2"
+            date={dates.secondPractice.date}
+            time={dates.secondPractice.time}
+            type="practice"
+          />
+          {
+            weekend.ThirdPractice && (
+              <Session
+                title="FP3"
+                date={dates.thirdPractice!.date}
+                time={dates.thirdPractice!.time}
+                type="practice"
+              />
+            )
+          }
+          {
+            weekend.Sprint && (
+              <Session
+                title="Sprint"
+                date={dates.sprint!.date}
+                time={dates.sprint!.time}
+                type="sprint"
+              />
+            )
+          }
+          <Session
+            title="QU"
+            date={dates.qualifying.date}
+            time={dates.qualifying.time}
+            type="qualifying"
+          />
+          <Session
+            title="RACE"
+            date={dates.race.date}
+            time={dates.race.time}
+            type="race"
+          />
+        </ul>
       )}
     </div>
   );
