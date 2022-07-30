7bf322ed23a669e95099ba5fd249110c4421b08e
XcQuid <creed.35@yandex.ru>
2022-07-30 21:08:53 +0300

    fix: change driverInfo component display conditions

--- src/components/DriverInfo/DriverInfo.tsx
+++ src/components/DriverInfo/DriverInfo.tsx
@@ -1,4 +1,6 @@
 import React, { useEffect } from 'react';
+import { parseISO, format } from 'date-fns';
+import { findFlagUrlByNationality } from 'country-flags-svg';
 
 import './DriverInfo.scss';
 import { useTypedSelector } from '../../hooks/useTypedSelector';
@@ -15,29 +17,42 @@ const DriverInfo: React.FC = () => {
     driverError,
   } = useTypedSelector((state) => state.driver);
 
+  const flagUrl = driverInfo && findFlagUrlByNationality(driver.nationality);
+
   useEffect(() => {
+    console.log(driver);
     if (driver) {
-      fetchDriver(`${driver.givenName} ${driver.familyName}`);
+      fetchDriver(driver.driverId.replace('_', ' '));
     }
   }, [driver]);
 
   return (
-    <div className="driver-info">
-      <img
-        className="driver-info__image"
-        src={driverInfo?.image}
-        alt={driverInfo?.name}
-      />
-      <h3 className="driver-info__header">{driverInfo?.name}</h3>
-      <div className="driver-info__container">
-        <p className="driver-info__parameter">
-          Nationality: <span>{driverInfo?.nationality}</span>
-        </p>
-        <p className="driver-info__parameter">
-          Birth date: <span>{driverInfo?.birthdate}</span>
-        </p>
+    driverInfo
+    && (
+      <div className="driver-info">
+        <img
+          className="driver-info__image"
+          src={driverInfo?.image}
+          alt={driverInfo?.name}
+        />
+        <h3 className="driver-info__header">{driverInfo?.name}</h3>
+        <div className="driver-info__container">
+          <p className="driver-info__parameter">
+            Nationality:
+            <span>
+              <img className="driver-info__flag" src={flagUrl} alt="flag" />
+              {driverInfo?.nationality}
+            </span>
+          </p>
+          <p className="driver-info__parameter">
+            Birth date:
+            <span>
+              {format(parseISO(driverInfo!.birthdate), 'd MMM yyyy')}
+            </span>
+          </p>
+        </div>
       </div>
-    </div>
+    )
   );
 };
 
