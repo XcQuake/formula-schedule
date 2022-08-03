d5379a17f061baba0ba4a270e5117188077e1735
XcQuid <creed.35@yandex.ru>
2022-08-03 11:57:19 +0300

    refactor: move jsx elements to separate functions in WeekendInfo component

--- src/components/WeekendInfo/WeekendInfo.tsx
+++ src/components/WeekendInfo/WeekendInfo.tsx
@@ -6,7 +6,6 @@ import placeholder from '../../images/F1-logo.svg';
 import { refactorWeekendDates } from '../../utils/utils';
 import { useTypedSelector } from '../../hooks/useTypedSelector';
 import { useActions } from '../../hooks/useActions';
-import { Race } from '../../models/ergastApiTypes';
 import Session from '../Session/Session';
 import ResultsList from '../ResultsList/ResultsList';
 import Preloader from '../Preloader/Preloader';
@@ -19,7 +18,6 @@ const WeekendInfo: React.FC = () => {
   const {
     resultLoading,
     raceResult,
-    resultError,
   } = useTypedSelector((state) => state.result);
 
   const wikiTitle = weekend?.raceName.replace(' ', '_');
@@ -38,102 +36,105 @@ const WeekendInfo: React.FC = () => {
         fetchWikiImage(wikiTitle!);
       }
     }
-    console.log(wikiImage);
   }, [weekend]);
 
+  const renderCircuitInfo: JSX.Element = (
+    <>
+      <figure className="weekend-info__figure">
+        <img
+          className="weekend-info__image"
+          src={wikiImage || placeholder}
+          alt={weekend?.Circuit.circuitName}
+        />
+        <h5 className="weekend-info__circuit-name">
+          {weekend?.Circuit.circuitName}
+        </h5>
+      </figure>
+      <div className="weekend-info__circuit-description">
+        <p className="weekend-info__circuit-info">
+          Country: <span>{weekend?.Circuit.Location.country}</span>
+        </p>
+        <p className="weekend-info__circuit-info">
+          Locality: <span>{weekend?.Circuit.Location.locality}</span>
+        </p>
+      </div>
+    </>
+  );
+
+  const renderSessionDates: JSX.Element | null = weekendInfo && (
+    <ul className="weekend-info__sessions">
+      <Session
+        title="FP1"
+        date={weekendInfo.dates.firstPractice.date}
+        time={weekendInfo.dates.firstPractice.time}
+        type="practice"
+      />
+      <Session
+        title="FP2"
+        date={weekendInfo.dates.secondPractice.date}
+        time={weekendInfo.dates.secondPractice.time}
+        type="practice"
+      />
+      {
+        weekend.ThirdPractice && (
+          <Session
+            title="FP3"
+            date={weekendInfo.dates.thirdPractice.date}
+            time={weekendInfo.dates.thirdPractice.time}
+            type="practice"
+          />
+        )
+      }
+      {
+        weekend.Sprint && (
+          <Session
+            title="Sprint"
+            date={weekendInfo.dates.sprint.date}
+            time={weekendInfo.dates.sprint.time}
+            type="sprint"
+          />
+        )
+      }
+      <Session
+        title="QU"
+        date={weekendInfo.dates.qualifying.date}
+        time={weekendInfo.dates.qualifying.time}
+        type="qualifying"
+      />
+      <Session
+        title="RACE"
+        date={weekendInfo.dates.race.date}
+        time={weekendInfo.dates.race.time}
+        type="race"
+      />
+    </ul>
+  );
+
   return (
-    <div className="weekend-info">
-      <h3 className="weekend-info__header">{weekend?.raceName}</h3>
-      {
-        weekend
-        && !weekendInfo!.isOver
-        && (
-        <>
-          <figure className="weekend-info__figure">
-            <img
-              className="weekend-info__image"
-              src={wikiImage || placeholder}
-              alt={weekend?.Circuit.circuitName}
-            />
-            <h5 className="weekend-info__circuit-name">
-              {weekend?.Circuit.circuitName}
-            </h5>
-          </figure>
-          <div className="weekend-info__circuit-description">
-            <p className="weekend-info__circuit-info">
-              Country: <span>{weekend?.Circuit.Location.country}</span>
-            </p>
-            <p className="weekend-info__circuit-info">
-              Locality: <span>{weekend?.Circuit.Location.locality}</span>
-            </p>
-          </div>
-        </>
-        )
-      }
-      {
-        weekend
-        && !weekendInfo!.isOver
-        && (
-        <ul className="weekend-info__dates">
-          <Session
-            title="FP1"
-            date={weekendInfo!.dates.firstPractice.date}
-            time={weekendInfo!.dates.firstPractice.time}
-            type="practice"
-          />
-          <Session
-            title="FP2"
-            date={weekendInfo!.dates.secondPractice.date}
-            time={weekendInfo!.dates.secondPractice.time}
-            type="practice"
-          />
-          {
-            weekend.ThirdPractice && (
-              <Session
-                title="FP3"
-                date={weekendInfo!.dates.thirdPractice!.date}
-                time={weekendInfo!.dates.thirdPractice!.time}
-                type="practice"
-              />
-            )
-          }
-          {
-            weekend.Sprint && (
-              <Session
-                title="Sprint"
-                date={weekendInfo!.dates.sprint!.date}
-                time={weekendInfo!.dates.sprint!.time}
-                type="sprint"
-              />
-            )
-          }
-          <Session
-            title="QU"
-            date={weekendInfo!.dates.qualifying.date}
-            time={weekendInfo!.dates.qualifying.time}
-            type="qualifying"
-          />
-          <Session
-            title="RACE"
-            date={weekendInfo!.dates.race.date}
-            time={weekendInfo!.dates.race.time}
-            type="race"
-          />
-        </ul>
-        )
-      }
-      {
-        weekendInfo?.isOver
-        && resultLoading
-        && <Preloader />
-      }
-      {
-        weekendInfo?.isOver
-        && !resultLoading
-        && raceResult
-        && <ResultsList results={raceResult} />
-      }
-    </div>
+    weekend && weekendInfo && (
+      <div className="weekend-info">
+        <h3 className="weekend-info__header">{weekend.raceName}</h3>
+        {
+          !weekendInfo.isOver
+          && renderCircuitInfo
+        }
+        {
+          !weekendInfo.isOver
+          && renderSessionDates
+        }
+        {
+          weekendInfo.isOver
+          && resultLoading
+          && <Preloader />
+        }
+        {
+          weekendInfo.isOver
+          && !resultLoading
+          && raceResult
+          && <ResultsList results={raceResult} />
+        }
+      </div>
+    )
   );
 };
 
