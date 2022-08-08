54e47b981af0d6d04228e5b8a42a19f73355dd6d
XcQuid <creed.35@yandex.ru>
2022-08-08 18:23:30 +0300

    feat: add mouse click handler outside of popup

--- src/components/Popup/Popup.tsx
+++ src/components/Popup/Popup.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useActions } from '../../hooks/useActions';
 import { useTypedSelector } from '../../hooks/useTypedSelector';
 
@@ -7,10 +7,23 @@ import './Popup.scss';
 const Popup: React.FC = () => {
   const { closePopup } = useActions();
   const { isOpen, content } = useTypedSelector((state) => state.popup);
+  const ref = useRef<HTMLDivElement>(null);
+
+  const handleClickOutside = (evt: React.MouseEvent<HTMLDivElement>): void => {
+    const target = evt.target as HTMLDivElement;
+    if (!ref.current!.contains(target)) {
+      closePopup();
+    }
+  };
 
   return (
-    <div className={`popup ${isOpen && 'popup_open'}`}>
-      <div className="popup__wrapper">
+    <div
+      className={`popup ${isOpen && 'popup_open'}`}
+      onClick={(evt) => handleClickOutside(evt)}
+      role="button"
+      tabIndex={0}
+    >
+      <div className="popup__wrapper" ref={ref}>
         <button
           className="popup__close-button"
           aria-label="close"
