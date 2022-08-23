2956b1be45b82986f58e5ff42af88a96ce4bfbee
XcQuid <creed.35@yandex.ru>
2022-08-23 12:53:50 +0300

    feat: add cursor follower component

--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -1,154 +1,81 @@
-/* eslint-disable max-len */
-/* eslint-disable prefer-const */
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 import './Canvas.scss';
 
-interface Point {
-  x: number;
-  y: number;
-}
-
 const Canvas: React.FC = () => {
-  const containerRef = useRef<HTMLDivElement>(null);
-  const carRef = useRef<HTMLDivElement>(null);
-  const container = containerRef.current;
-  const car = carRef.current;
+  const carRef = React.useRef<HTMLDivElement>(null);
+  const positionRef = useState({
+    mouseX: 0,
+    mouseY: 0,
+    destinationX: 0,
+    destinationY: 0,
+    distanceX: 0,
+    distanceY: 0,
+    key: -1,
+    windowHeight: 0,
+    windowWidth: 0,
+  });
 
-  let positions = {
-    current: { x: 0, y: 0 },
-    target: { x: 0, y: 0 },
-    working: { x: 0, y: 0 },
+  const mouseHandler = (event: MouseEvent): void => {
+    const { clientX, clientY } = event;
+    const mouseX = clientX;
+    const mouseY = clientY;
+    console.log(mouseX);
+    if (carRef) {
+      positionRef.current.mouseX = mouseX - carRef!.current!.clientWidth / 2;
+      positionRef.current.mouseY = mouseY - carRef!.current!.clientHeight / 2;
+    }
   };
 
-  let limX = 0;
-  let limY = 0;
-  let containerX = 0;
-  let containerY = 0;
-  let inertia = 0.3;
-  let firstRun = true;
-
-  function mouseMoveHandler(e: MouseEvent): void {
-    positions.target.x = e.clientX - containerX;
-    positions.target.y = e.clientY - containerY;
-
-    if (positions.target.x > limX) positions.target.x = limX;
-    if (positions.target.y > limY) positions.target.y = limY;
-  }
-
-  function inertiaTo(current: number, target: number, amount: number): number {
-    if (amount === 1) return target;
-    let distToGo = target - current;
-    let delta = current + (distToGo + amount);
-    if (Math.abs(distToGo) < 0.01) {
-      distToGo = 0;
-      delta = target;
-    }
-
-    return delta;
-  }
-
-  function update(): void {
-    const { current, target, working } = positions;
-    if (firstRun) {
-      firstRun = false;
-    } else {
-      current.x = working.x;
-      current.y = working.y;
-    }
-
-    working.x = inertiaTo(current.x, target.x, inertia);
-    working.y = inertiaTo(current.y, target.y, inertia);
-
-    car!.style.left = `${working.x}px`;
-    car!.style.top = `${working.y}px`;
-
-    requestAnimationFrame(update);
-  }
-
-  function init(): void {
-    console.log('foo');
-    containerX = window.scrollX + container!.getBoundingClientRect().left;
-    containerY = window.scrollY + container!.getBoundingClientRect().top;
-
-    limX = container!.getBoundingClientRect().width - car!.getBoundingClientRect().width;
-    limY = container!.getBoundingClientRect().height - car!.getBoundingClientRect().height;
-
-    positions.current.x = parseFloat(getComputedStyle(car!).left);
-    positions.current.y = parseFloat(getComputedStyle(car!).left);
-
-    container?.addEventListener('mousemove', mouseMoveHandler);
-
-    update();
-  }
-
   useEffect(() => {
-    if (container && car) init();
-  }, [container, car]);
+    document.addEventListener('mousemove', (event) => mouseHandler(event));
 
-  // useEffect(() => {
-  //   document.addEventListener('mousemove', (event) => {
-  //     const { clientX, clientY } = event;
-  //     const mouseX = clientX;
-  //     const mouseY = clientY;
+    return () => {
+      document.removeEventListener('mousemove', mouseHandler);
+    };
+  }, []);
+  console.log(positionRef.current.mouseX);
+  useEffect(() => {
+    // positionRef.current.key = requestAnimationFrame(followMouse);
+    const {
+      destinationX,
+      destinationY,
+      distanceX,
+      distanceY,
+      mouseX,
+      mouseY,
+      windowHeight,
+      windowWidth,
+    } = positionRef.current;
 
-  //     if (carRef) {
-  //       positionRef.current.mouseX = mouseX - 5;
-  //       positionRef.current.mouseY = mouseY - 5;
-  //     }
-  //   });
+    if (!destinationX || !destinationY) {
+      positionRef.current.destinationX = mouseX;
+      positionRef.current.destinationY = mouseY;
+    } else {
+      positionRef.current.distanceX = (mouseX - destinationX) * 0.1;
+      positionRef.current.distanceY = (mouseY - destinationY) * 0.1;
+      if (
+        Math.abs(distanceX)
+        + Math.abs(distanceY)
+        < 0.1
+      ) {
+        positionRef.current.destinationX = mouseX;
+        positionRef.current.destinationY = mouseY;
+      } else {
+        positionRef.current.destinationX += distanceX;
+        positionRef.current.destinationY += distanceY;
+      }
+    }
 
-  // return () => {};
-  // });
-
-  // useEffect(() => {
-  //   const followMouse = (): void => {
-  //     positionRef.current.key = requestAnimationFrame(followMouse);
-  //     let {
-  //       destinationX,
-  //       destinationY,
-  //       distanceX,
-  //       distanceY,
-  //     } = positionRef.current;
-  //     const {
-  //       mouseX,
-  //       mouseY,
-  //     } = positionRef.current;
-
-  //     if (!destinationX || !destinationY) {
-  //       destinationX = mouseX;
-  //       destinationY = mouseY;
-  //     } else {
-  //       distanceX = (mouseX - destinationX) * 0.9;
-  //       distanceY = (mouseY - destinationY) * 0.9;
-
-  //       if (
-  //         Math.abs(distanceX)
-  //         + Math.abs(distanceY)
-  //         < 0.1
-  //       ) {
-  //         destinationX = mouseX;
-  //         destinationY = mouseY;
-  //       } else {
-  //         destinationX += distanceX;
-  //         destinationY += distanceY;
-  //       }
-  //     }
-  //     // eslint-disable-next-line max-len
-  //     carRef.current!.style.transform = `translate3d(${destinationX}px, ${destinationY}px, 0)`;
-  //   };
-
-  //   followMouse();
-  // }, []);
+    carRef.current!.style.top = `${destinationY}px`;
+    carRef.current!.style.left = `${destinationX}px`;
+  }, [positionRef.current.mouseX, positionRef.current.mouseY]);
 
   return (
-    <div className="canvas" ref={containerRef}>
-      {/* <canvas ref={canvasRef} style={{ width: '100%', height: '100%' }} /> */}
-      <span
-        className="car"
-        ref={carRef}
-      />
-    </div>
+    <div
+      className="tire"
+      ref={carRef}
+    />
   );
 };
 
