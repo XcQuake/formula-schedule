580267bf6e7cc35341e5ddf4cfd0a724c571fd29
XcQuid <creed.35@yandex.ru>
2022-09-01 12:22:05 +0300

    style: remove commnets

--- src/components/WeekendResults/WeekendResults.tsx
+++ src/components/WeekendResults/WeekendResults.tsx
@@ -6,10 +6,8 @@ import './WeekendResults.scss';
 import { Race } from '../../models/ergastApiTypes';
 import { useActions } from '../../hooks/useActions';
 import { useTypedSelector } from '../../hooks/useTypedSelector';
-import Dropdown from '../Dropdown/Dropdown';
 import RaceResults from '../RaceResults/RaceResults';
 import QualifyResults from '../QualifyResults/QualifyResults';
-import Preloader from '../Preloader/Preloader';
 
 interface Props {
   weekend: Race;
@@ -22,36 +20,15 @@ const WeekendResults: React.FC<Props> = ({ weekend }) => {
     raceResult,
     qualifyResult,
   } = useTypedSelector((state) => state.result);
-  // const { session } = useTypedSelector((state) => state.dropdown);
   const [prevEl, setPrevEl] = useState<HTMLElement | null>(null);
   const [nextEl, setNextEl] = useState<HTMLElement | null>(null);
   const [session, setSession] = useState('race');
-
-  // const dropDownOptions = [{
-  //   name: 'Race',
-  //   value: 'race',
-  // }, {
-  //   name: 'Qualify',
-  //   value: 'qualifying',
-  // }];
 
   useEffect(() => {
     if (session) {
       fetchRaceResult(weekend.season, weekend.round, session);
     }
   }, [session, weekend]);
-
-  // const renderResults = (): JSX.Element => {
-  //   if (!resultLoading) {
-  //     if (session === 'race' && raceResult) {
-  //       return <RaceResults results={raceResult} />;
-  //     }
-  //     if (session === 'qualifying' && qualifyResult) {
-  //       return <QualifyResults results={qualifyResult} />;
-  //     }
-  //   }
-  //   return <Preloader />;
-  // };
 
   const handleChangeSession = (): void => {
     if (session === 'race') setSession('qualifying');
@@ -60,10 +37,6 @@ const WeekendResults: React.FC<Props> = ({ weekend }) => {
 
   return (
     <div className="weekend-results">
-      {/* <Dropdown
-        title="Session"
-        options={dropDownOptions}
-      /> */}
       <div className="weekend-results__navigation">
         <div className="weekend-results__swiper-buttons">
           <button
@@ -102,7 +75,6 @@ const WeekendResults: React.FC<Props> = ({ weekend }) => {
           }
         </SwiperSlide>
       </Swiper>
-      {/* { session && renderResults() } */}
     </div>
   );
 };
